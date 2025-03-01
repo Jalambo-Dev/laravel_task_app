838e8515ed50ff0d52cf2ab40313c1d217f86398
Jalambo-Dev <ahmedjalambo.dev@gmail.com>
2025-03-01 22:03:59 +0200

    Refactor user routes and forms to use named routes for improved maintainability

--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,22 @@
 
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('tasks', [TaskController::class, 'index']);
-
-Route::post('tasks/create', [TaskController::class, 'create']);
-
-Route::post('tasks/delete/{id}', [TaskController::class, 'delete']);
-
-Route::get('tasks/edit/{id}', [TaskController::class, 'edit']);
-
-Route::post('tasks/update', [TaskController::class, 'update']);
-
+// Task Routes
+Route::get('tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::post('tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+Route::post('tasks/delete/{id}', [TaskController::class, 'delete'])->name('tasks.delete');
+Route::get('tasks/edit/{id}', [TaskController::class, 'edit'])->name('tasks.edit');
+Route::post('tasks/update', [TaskController::class, 'update'])->name('tasks.update');
 
 // User Routes
-Route::get('users', [UserController::class, 'index']);
-Route::post('users/create', [UserController::class, 'create']);
-Route::post('users/delete/{id}', [UserController::class, 'delete']);
-Route::get('users/edit/{id}', [UserController::class, 'edit']);
-Route::post('users/update', [UserController::class, 'update']);
+Route::get('users', [UserController::class, 'index'])->name('users.index');
+Route::post('users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('users/delete/{id}', [UserController::class, 'delete'])->name('users.delete');
+Route::get('users/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
+Route::post('users/update', [UserController::class, 'update'])->name('users.update');
