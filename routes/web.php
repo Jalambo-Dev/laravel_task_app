2024462aebc0b63182f3780abd4e1aabb51ff6ca
Jalambo-Dev <ahmedjalambo.dev@gmail.com>
2025-03-01 21:59:26 +0200

    Update routes to include 'tasks' and 'users' prefixes for better organization

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,18 @@ Route::get('/', function () {
 
 Route::get('tasks', [TaskController::class, 'index']);
 
-Route::post('create', [TaskController::class, 'create']);
+Route::post('tasks/create', [TaskController::class, 'create']);
 
-Route::post('delete/{id}', [TaskController::class, 'delete']);
+Route::post('tasks/delete/{id}', [TaskController::class, 'delete']);
 
-Route::get('edit/{id}', [TaskController::class, 'edit']);
+Route::get('tasks/edit/{id}', [TaskController::class, 'edit']);
 
-Route::post('update', [TaskController::class, 'update']);
+Route::post('tasks/update', [TaskController::class, 'update']);
 
 
 // User Routes
 Route::get('users', [UserController::class, 'index']);
-Route::post('create', [UserController::class, 'create']);
-Route::post('delete/{id}', [UserController::class, 'delete']);
-Route::get('edit/{id}', [UserController::class, 'edit']);
-Route::post('update', [UserController::class, 'update']);
+Route::post('users/create', [UserController::class, 'create']);
+Route::post('users/delete/{id}', [UserController::class, 'delete']);
+Route::get('users/edit/{id}', [UserController::class, 'edit']);
+Route::post('users/update', [UserController::class, 'update']);
