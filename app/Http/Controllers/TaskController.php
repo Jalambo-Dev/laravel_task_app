2d218fd905bdc6276691bf15427cd3d0a6b2c32d
Jalambo-Dev <ahmedjalambo.dev@gmail.com>
2025-03-01 21:27:00 +0200

    Refactor TaskController to use Eloquent for task retrieval and creation

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,16 +10,16 @@ class TaskController extends Controller
 {
     public function index()
     {
-        $tasks = DB::table('tasks')->get();
+        $tasks = Task::all();
         return view('tasks', compact('tasks'));
     }
 
     public function create()
     {
         $task_title = $_POST['title'];
-        DB::table('tasks')->insert([
-            'title' => $task_title
-        ]);
+        $task = new Task();
+        $task->title = $task_title;
+        $task->save();
         return redirect('/tasks');
     }
 
