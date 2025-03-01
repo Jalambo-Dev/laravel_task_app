ad2bd23004f2fdf03a7401660944e3be9a97e497
Jalambo-Dev <ahmedjalambo.dev@gmail.com>
2025-03-01 21:33:35 +0200

    Refactor TaskController to use Eloquent for task creation, deletion, editing, and updating

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
@@ -14,34 +13,38 @@ class TaskController extends Controller
         return view('tasks', compact('tasks'));
     }
 
-    public function create()
+    public function create(Request $request)
     {
-        $task_title = $_POST['title'];
         $task = new Task();
-        $task->title = $task_title;
+        $task->title = $request->input('title');
         $task->save();
         return redirect('/tasks');
     }
 
     public function delete($id)
     {
-        DB::table('tasks')->where('id', $id)->delete();
+        $task = Task::find($id);
+        if ($task) {
+            $task->delete();
+        }
         return redirect('/tasks');
     }
 
     public function edit($id)
     {
-        $task = DB::table('tasks')->where('id', $id)->first();
-        $tasks = DB::table('tasks')->get();
+        $task = Task::find($id);
+        $tasks = Task::all();
         return view('tasks', compact('task', 'tasks'));
     }
 
-    public function update()
+    public function update(Request $request)
     {
-        $id = $_POST['id'];
-        DB::table('tasks')->where('id', $id)->update([
-            'title' => $_POST['title']
-        ]);
+        $id = $request->input('id');
+        $task = Task::find($id);
+        if ($task) {
+            $task->title = $request->input('title');
+            $task->save();
+        }
         return redirect('/tasks');
     }
 }
